af96f9fb987bce7055a0630535612946f6ba4c9c
Wesley Ketchum <wketchum@fnal.gov>
2018-03-14 13:55:02 -0500

    refactor the refactoring to clean it up

--- larsim/Simulation/LArG4Parameters_service.cc
+++ larsim/Simulation/LArG4Parameters_service.cc
@@ -61,7 +61,9 @@ namespace sim {
     fFillSimEnergyDeposits       = pset.get< bool                 >("FillSimEnergyDeposits",false);
     if(fFillSimEnergyDeposits)
       fInitialSimEnergyDepositSize = pset.get< size_t               >("InitialSimEnergyDepositSize");
-
+    fNoElectronPropagation   = pset.get< bool                     >("NoElectronPropagation",false);
+    fNoPhotonPropagation     = pset.get< bool                     >("NoPhotonPropagation",false);
+    
     return;
   }
 
